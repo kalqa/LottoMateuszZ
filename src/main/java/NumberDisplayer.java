dcfc3bff12b87134487a2a6cb4baa61ba0a9d0d1
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2022-10-19 12:11:19 +0200

    first commit

---
+++ src/main/java/NumberDisplayer.java
@@ -0,0 +1,8 @@
+public class NumberDisplayer {
+    public void displayUserNumber(int[] userNumber) {
+        System.out.println("Your numbers are:");
+        for (int i : userNumber) {
+            System.out.print(i + " ");
+        }
+    }
+}
